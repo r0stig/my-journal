2ee8e06cfa60ab78d65ba49ff6258210d477b0f7
r0stig <pr_125@hotmail.com>
2021-12-11 10:29:23 +0100

    feat: show days with items as marked

--- client/src/pages/calendar/calendar-styles.tsx
+++ client/src/pages/calendar/calendar-styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
   padding: 10px;
@@ -8,10 +8,17 @@ export const WeekdayContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   gap: 10px;
+  margin-bottom: 10px;
 `
 
-export const Day = styled.div`
+interface DayProps {
+  marked?: boolean
+}
 
+export const Day = styled.div<DayProps>`
+  ${props => props.marked && css`
+    font-weight: 600;
+  `}
 `
 
 interface DaysContainerProps {
--- client/src/pages/calendar/calendar.tsx
+++ client/src/pages/calendar/calendar.tsx
@@ -1,21 +1,37 @@
 import React from 'react'
-import { getDay } from 'date-fns'
+import { getDay, getDaysInMonth } from 'date-fns'
 import { Container, Day, DaysContainer, WeekdayContainer, WeekdayHeader } from './calendar-styles'
+import { getEntries } from '../../lib/store'
 
+function leftPad(str: string, padding: string, width: number): string {
+  if (!str) return str
+  if (str.length >= width) return str
+  let retStr = ''
+  for (let i = 0; i < width - str.length; i++) {
+    retStr += padding
+  }
+  return retStr + str
+}
 
 export const Calendar: React.FC<{}> = () => {
   const [year, setYear] = React.useState<number>(2021)
   const [month, setMonth] = React.useState<number>(11)
+  const [days, setDays] = React.useState<Array<{date: number, hasItems: boolean}>>([])
 
-  let days = []
-  for (let i = 1; i < 30; i++) {
-    days.push({
-      date: i,
-      hasItems: false
+  React.useEffect(() => {
+    getEntries().then((entries) => {
+      let days = []
+      for (let i = 1; i <= getDaysInMonth(new Date(year, month)); i++) {
+        days.push({
+          date: i,
+          hasItems: entries.some((entry) => entry.day === `${year}-${month+1}-${leftPad(String(i), '0', 2)}`)
+        })
+      }
+      setDays(days)
     })
-  }
-  const firstDayOfWeek = getDay(new Date(year, month, 1))
-  console.log('first day of week', firstDayOfWeek)
+  }, [year, month])
+
+ const firstDayOfWeek = getDay(new Date(year, month, 1))
   return (
     <Container>
       <WeekdayContainer>
@@ -29,7 +45,7 @@ export const Calendar: React.FC<{}> = () => {
       </WeekdayContainer>
       <DaysContainer startDay={firstDayOfWeek}>
         {days.map((day) => (
-          <Day>{day.date}</Day>
+          <Day marked={day.hasItems}>{day.date}</Day>
         ))}
       </DaysContainer>
     </Container>
