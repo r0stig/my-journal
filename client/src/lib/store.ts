005c8e2c39ee7747d2b608ee6b1c131ca9c3a821
r0stig <pr_125@hotmail.com>
2021-12-15 21:06:54 +0100

    wip: encryption and indexedDb

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -4,14 +4,92 @@ export type Entry = {
   content: string
 }
 
+type EncryptedEntry = {
+  day: string
+  payload: any
+}
+
+let memoryDb: Entry[] = []
+
 const DB_NAME = 'my-journal'
 const DB_VERSION = 1
 const ENTRIES_OBJECT_STORE = "entries"
 let db: IDBDatabase
+let dbKey: CryptoKey
 // let entriesObjectStore: IDBObjectStore
+
+async function sha256(value: string): Promise<ArrayBuffer> {
+  return await crypto.subtle.digest(
+    'SHA-256',
+    new TextEncoder().encode(value)
+  )
+}
+
+async function getKey(password: string): Promise<CryptoKey> {
+  return await crypto.subtle.importKey(
+    'raw',
+    await sha256(password),
+    {name: 'AES-GCM'},
+    false,
+    ['encrypt', 'decrypt']
+  )
+}
+
+const iv = crypto.getRandomValues(new Uint8Array(12))
+console.log('iv is', iv)
+// const ivStr = String.fromCharCode.apply(null, new Uint8Array(iv) as any)
+const ivStr = ":&Q¯öû¯o»Q"
+console.log('ivStr:"' + ivStr + '"')
+
+async function encrypt(key: CryptoKey, data: string): Promise<string> {
+
+  const encrypted = await crypto.subtle.encrypt(
+    {
+      iv: str2ab(ivStr),
+      name: 'AES-GCM'
+    },
+    key,
+    new TextEncoder().encode(data)
+  )
+  // return encrypted
+  return String.fromCharCode.apply(null, new Uint8Array(encrypted) as any)
+  // return btoa(String.fromCharCode.apply(null, new Uint8Array(encrypted) as any))
+  // return btoa(String.fromCharCode(...new Uint8Array(encrypted)))
+  // return bufferToBase64(encrypted)
+}
+
+function str2ab(str: string): ArrayBuffer {
+  var buf = new ArrayBuffer(str.length)
+  var bufView = new Uint8Array(buf)
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i)
+  }
+  return buf
+}
+
+async function decrypt(key: CryptoKey, data: string): Promise<string> {
+  //let asciiString = atob(data)
+  //const bb = new Uint8Array(Array.from(asciiString).map(char => char.charCodeAt(0)));
+  //console.log('bb', bb)
+  try {
+    const decrypted = await crypto.subtle.decrypt(
+      {
+        iv: str2ab(ivStr),
+        name: 'AES-GCM'
+      },
+      key,
+      str2ab(data)
+    )
+    return new TextDecoder().decode(decrypted)
+  } catch (err) {
+    console.log('eerr...', err)
+    return ""
+  }
+}
 
 export function initStore(): Promise<void> {
   return new Promise((resolve, reject) => {
+
     const request = window.indexedDB.open(DB_NAME, DB_VERSION)
 
     request.onupgradeneeded = (event: any) => {
@@ -28,12 +106,52 @@ export function initStore(): Promise<void> {
       db = event.target.result
       resolve()
     }
+
+
+    getKey('abc123').then(async (key) => {
+      dbKey = key
+
+      const savedDb = window.localStorage.getItem(DB_NAME)
+      if (savedDb) {
+        const decrypted = await decrypt(key, savedDb)
+        memoryDb = JSON.parse(decrypted)
+      }
+
+      resolve()
+    })
   })
 }
 
 export function storeEntry(entry: Entry): Promise<void> {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    console.log('store...')
+
+    const curEntry = memoryDb.find((e) => e.day === entry.day)
+    if (curEntry) {
+      memoryDb = memoryDb.map((e) => {
+        if (e.day === entry.day) {
+          return entry
+        }
+        return e
+      })
+    } else {
+      memoryDb = [
+        ...memoryDb,
+        entry
+      ]
+    }
+
+    const data = await encrypt(dbKey, JSON.stringify(memoryDb))
+    // console.log('encrypted to save', data, JSON.stringify(memoryDb))
+    window.localStorage.setItem(DB_NAME, data)
+
+
+    //resolve()
+
+
+
     console.log('add thingy to database')
+    const encryptedEntry = await encrypt(dbKey, JSON.stringify(entry))
     const transaction = db.transaction([ENTRIES_OBJECT_STORE], "readwrite")
 
     transaction.onerror = (event) => {
@@ -47,18 +165,36 @@ export function storeEntry(entry: Entry): Promise<void> {
     const entriesObjectStore = transaction.objectStore(ENTRIES_OBJECT_STORE)
     console.log('doing add...')
 
-    const putRequest = entriesObjectStore.put(entry)
+
+    console.log('before put..', {
+      day: entry.day,
+      payload: encryptedEntry
+    })
+    const putRequest = entriesObjectStore.put({
+      day: entry.day,
+      payload: encryptedEntry
+    })
     putRequest.onerror = (event: any) => {
       reject(new Error('Error putting data: ' + event.target.error))
     }
     putRequest.onsuccess = () => {
       resolve()
     }
+
   })
 }
 
 export function getEntry(day: string): Promise<Entry> {
   return new Promise((resolve, reject) => {
+    /*
+    const entry = memoryDb.find((entry) => entry.day === day)
+    if (entry) {
+      resolve(entry)
+    } else {
+      reject(new Error('Not found'))
+    }
+    */
+
     const transaction = db.transaction([ENTRIES_OBJECT_STORE], "readwrite")
 
     transaction.onerror = (event) => {
@@ -77,15 +213,23 @@ export function getEntry(day: string): Promise<Entry> {
       reject(new Error('Error fetching all: ' + event.target.error))
     }
 
-    getAllRequest.onsuccess = (event: any) => {
+    getAllRequest.onsuccess = async (event: any) => {
       const result = event.target.result
-      resolve(result)
+      console.log('get reuslt', result)
+
+        const decrypted = await decrypt(dbKey, result.payload)
+        const entry: Entry = JSON.parse(decrypted)
+        resolve(entry)
+
     }
+
   })
 }
 
 export function getEntries(): Promise<Entry[]> {
   return new Promise((resolve, reject) => {
+    //resolve(JSON.parse(JSON.stringify(memoryDb)))
+
     const transaction = db.transaction([ENTRIES_OBJECT_STORE], "readwrite")
 
     transaction.onerror = (event) => {
@@ -104,10 +248,20 @@ export function getEntries(): Promise<Entry[]> {
       reject(new Error('Error fetching all: ' + event.target.error))
     }
 
-    getAllRequest.onsuccess = (event: any) => {
+    getAllRequest.onsuccess = async (event: any) => {
       const result = event.target.result
-      resolve(result)
+      const decrypted: Entry[] = []
+      await Promise.all(result.map(async (res: unknown) => {
+        if ((res as EncryptedEntry).payload) {
+          const payload = await decrypt(dbKey, (res as EncryptedEntry).payload)
+          decrypted.push(JSON.parse(payload))
+        } else {
+          decrypted.push(res as Entry)
+        }
+      }))
+      resolve(decrypted)
     }
+
   })
 }
 
@@ -124,7 +278,6 @@ export function exportStore(): Promise<void> {
     }
 
     const entriesObjectStore = transaction.objectStore(ENTRIES_OBJECT_STORE)
-
     const getAllRequest = entriesObjectStore.getAll()
 
     getAllRequest.onerror = (event: any) => {
@@ -135,7 +288,9 @@ export function exportStore(): Promise<void> {
       const result = event.target.result
       console.log('get all result', result)
 
-      fetch('http://localhost:8080/store', {
+      const url = 'http://journal.swedishcolander.com/store'
+      // const url = 'http://localhost:8080/store'
+      fetch(url, {
         method: 'PUT',
         body: JSON.stringify(result),
         headers: {
@@ -147,5 +302,6 @@ export function exportStore(): Promise<void> {
         reject(err)
       })
     }
+
   })
 }
